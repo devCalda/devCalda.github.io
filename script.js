8094052f8a6b565d7bd22a286507932c4175d2dc
devCalda <116749753+devCalda@users.noreply.github.com>
2023-04-11 15:05:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const recordButton = document.getElementById('recordButton');
+const stopButton = document.getElementById('stopButton');
+const downloadButton = document.getElementById('downloadButton');
+const status = document.getElementById('status');
+
+let mediaRecorder;
+let recordedBlobs;
+
+function handleDataAvailable(event) {
+    if (event.data && event.data.size > 0) {
+        recordedBlobs.push(event.data);
+    }
+}
+
+recordButton.addEventListener('click', async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    recordedBlobs = [];
+    mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/webm' });
+
+    recordButton.disabled = true;
+    stopButton.disabled = false;
+    downloadButton.disabled = true;
+    status.textContent = 'Recording...';
+
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start(10);
+});
+
+stopButton.addEventListener('click', () => {
+    mediaRecorder.stop();
+    recordButton.disabled = false;
+    stopButton.disabled = true;
+    downloadButton.disabled = false;
+    status.textContent = 'Recording stopped. Click Download to save the file.';
+});
+
+downloadButton.addEventListener('click', () => {
+    const blob = new Blob(recordedBlobs, { type: 'audio/mp4' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'voice_recording.mp4';
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(() => {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 100);
+});
